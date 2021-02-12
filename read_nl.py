d00577c250ef9ed47b5b91c3f976cfdbd970cd7e
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-02-12 13:16:51 -0800

    Update read_nl.py

--- read_nl.py
+++ read_nl.py
@@ -11,21 +11,19 @@ def read_nl( ):
         if "#" in line:
             pass
         elif "np_begin" in line:
-            exec( line )
-            print(line)
-            print(np_begin)
+            exec( "global np_begin; " + line )
             nml_opts["np_begin"] = int(np_begin)
         elif "np_end" in line:
-            exec( line )
+            exec( "global np_end; " + line )
             nml_opts["np_end"] = int(np_end)
         elif "np_temp" in line:
-            exec( line )
+            exec( "global np_temp; " + line )
             nml_opts["np_temp"] = float(np_temp)
         elif "dp_temp" in line:
-            exec( line )
+            exec( "global dp_temp; " + line )
             nml_opts["dp_temp"] = float(dp_temp)
         elif "thresh" in line:
-            exec( line )
+            exec( "global thresh; " + line )
             nml_opts["thresh"] = float(thresh)
             
     return nml_opts
