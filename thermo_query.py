5019cda156e7a797e5e71ae3b244af7f42500f48
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-02-12 13:20:02 -0800

    Update thermo_query.py
    Variable name bug

--- thermo_query.py
+++ thermo_query.py
@@ -5,14 +5,14 @@ def query( hr, mn, T, outpt ):
     
     # Reads namelist every time, allowing user to
     # change settings without halting operation
-    nl_opts = nl.read_nl()
+    nml_opts = nl.read_nl()
     
     # Thresholds and time dependencies
     # Should use minutes at some point
-    if hr >= nl_opts["np_begin"] or hr < nl_opts["np_end"]:
-        TT = nl_opts["np_temp"]
+    if hr >= nml_opts["np_begin"] or hr < nml_opts["np_end"]:
+        TT = nml_opts["np_temp"]
     else:
-        TT = nl_opts["dp_temp"]
+        TT = nml_opts["dp_temp"]
     
     # If output is True/False, check threshold
     if outpt:
