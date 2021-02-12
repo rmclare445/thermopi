1d40cc95105f0ece9ee1b48df8b4f38cf981dd81
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-02-11 16:17:55 -0800

    Update thermo.py

--- thermo.py
+++ thermo.py
@@ -21,24 +21,21 @@ wl.write_log(" T(F) | H(%) | Time")
 
 try:
     while True:
-        
         # Retrieve humidity, temperature, and local time
         hum, temp = ad.read_retry(ad.DHT22, 4)
         temp = C_to_F( temp )
-        ti = time.localtime()
-        print(temp, temps)
+        lt = time.localtime()
+        
         if pert( temp, temps ) < 3.:
-            stat = tq.query( ti[3], ti[4], temp, 
+            stat = tq.query( lt[3], lt[4], temp, 
                              GPIO.input(18) )
-            print(stat)
-            if stat == True:
-                GPIO.output(18, True)
-            elif stat == False:
-                GPIO.output(18, False)
+            
+            try:
+                GPIO.output(18, stat)
         
         # Write state and times to log
-        wl.write_log(" %0.1f | %02d   | %02d%02d%02d" \
-                    % (temp, hum, ti[3], ti[4], ti[5]))
+        wl.write_log(" %0.1f | %02d   | %02d:%02d:%02d" \
+                    % ( temp, hum, lt[3], lt[4], lt[5] ))
         
         # Add new temp, delete oldest
         temps = update( temp, temps )
