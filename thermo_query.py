6877370c9e9708cb54d96994be91a1b096d4aee7
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-02-12 01:47:12 -0800

    Update thermo_query.py
    Add namelist suggestion

--- thermo_query.py
+++ thermo_query.py
@@ -3,6 +3,9 @@ def query( hr, mn, T, opt ):
     
     # Thresholds and time dependencies
     # Should use minutes at some point
+    ## Should really use a namelist for schedule
+    ## hours as well as temperatures; this would
+    ## enable altering these settings during operation
     if hr >= 22 or hr < 7:
         TT = 66.
     else:
