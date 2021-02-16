2383da4c0f13d4f6cc3514d911840f03398f9ce1
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-02-15 18:06:47 -0800

    Add minutes

--- read_nl.py
+++ read_nl.py
@@ -10,24 +10,30 @@ def read_nl( ):
         # Must add new elif for each new nl variable
         if "#" in line:
             pass
-        elif "np_begin" in line:
-            exec( "global np_begin; " + line )
-            nml_opts["np_begin"] = int(np_begin)
-        elif "np_end" in line:
-            exec( "global np_end; "   + line )
-            nml_opts["np_end"]   = int(np_end)
+        elif "np_begin_hr" in line:
+            exec( "global np_begin_hr; " + line )
+            nml_opts["np_begin_hr"] = int(np_begin_hr)
+        elif "np_begin_mn" in line:
+            exec( "global np_begin_mn; " + line )
+            nml_opts["np_begin_mn"] = int(np_begin_mn)
+        elif "np_end_hr" in line:
+            exec( "global np_end_hr; "   + line )
+            nml_opts["np_end_hr"]   = int(np_end_hr)
+        elif "np_end_mn" in line:
+            exec( "global np_end_mn; "   + line )
+            nml_opts["np_end_mn"]   = int(np_end_mn)
         elif "np_temp" in line:
-            exec( "global np_temp; "  + line )
-            nml_opts["np_temp"]  = float(np_temp)
+            exec( "global np_temp; "     + line )
+            nml_opts["np_temp"]     = float(np_temp)
         elif "dp_temp" in line:
-            exec( "global dp_temp; "  + line )
-            nml_opts["dp_temp"]  = float(dp_temp)
+            exec( "global dp_temp; "     + line )
+            nml_opts["dp_temp"]     = float(dp_temp)
         elif "up_tol" in line:
-            exec( "global up_tol; "   + line )
-            nml_opts["up_tol"]   = float(up_tol)
+            exec( "global up_tol; "      + line )
+            nml_opts["up_tol"]      = float(up_tol)
         elif "dn_tol" in line:
-            exec( "global dn_tol; "   + line )
-            nml_opts["dn_tol"]   = float(dn_tol)
+            exec( "global dn_tol; "      + line )
+            nml_opts["dn_tol"]      = float(dn_tol)
             
     return nml_opts
 
