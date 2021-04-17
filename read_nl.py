8c8a51988e0e91e04454b2e3e411fa2aa236622b
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-04-17 01:01:20 -0700

    Update read_nl.py

--- read_nl.py
+++ read_nl.py
@@ -11,26 +11,26 @@ def read_nl( ):
         if "#" in line:
             pass
         elif "n_phase" in line:
-            exec( "global n_phase; "     + line )
-            nml_opts["n_phase"]     = int(n_phase)
+            exec( "global n_phase; "   + line )
+            nml_opts["n_phase"]   = int(n_phase)
         elif "phase_hr" in line:
-            exec( "global phase_hr; "    + line )
-            nml_opts["phase_hr"]     = int(phase_hr)
+            exec( "global phase_hr; "  + line )
+            nml_opts["phase_hr"]  = phase_hr
         elif "phase_min" in line:
-            exec( "global phase_min; "   + line )
-            nml_opts["phase_min"]     = int(phase_min)
+            exec( "global phase_min; " + line )
+            nml_opts["phase_min"] = phase_min
         elif "phase_T" in line:
-            exec( "global phase_T; "     + line )
-            nml_opts["phase_T"]     = int(phase_T)
+            exec( "global phase_T; "   + line )
+            nml_opts["phase_T"]   = phase_T
         elif "up_tol" in line:
-            exec( "global up_tol; "      + line )
-            nml_opts["up_tol"]      = float(up_tol)
+            exec( "global up_tol; "    + line )
+            nml_opts["up_tol"]    = float(up_tol)
         elif "dn_tol" in line:
-            exec( "global dn_tol; "      + line )
-            nml_opts["dn_tol"]      = float(dn_tol)
+            exec( "global dn_tol; "    + line )
+            nml_opts["dn_tol"]    = float(dn_tol)
         elif "freq" in line:
-            exec( "global freq; "        + line )
-            nml_opts["freq"]        = float(freq)
+            exec( "global freq; "      + line )
+            nml_opts["freq"]      = float(freq)
             
     return nml_opts
     
