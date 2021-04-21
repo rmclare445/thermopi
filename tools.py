39ad9cdf1e38e2145400b95e8af7a6bc4132402f
Ryan Clare <52092909+rmclare445@users.noreply.github.com>
2021-04-21 01:58:53 -0700

    Update tools.py
    Add function descriptions

--- tools.py
+++ tools.py
@@ -1,9 +1,12 @@
 def C_to_F( C ):
+    # Convert Celsius to Fahrenheit
     return C * (9/5.) + 32.
     
 def pert( q, Qs ):
+    # Perturbation magnitude from mean
     return abs(q - (sum(Qs)/len(Qs)))
     
 def update( t, tt ):
+    # Store new value, discard oldest
     tt.append(t)
     return tt[1:]
