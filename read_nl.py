bcc8aa6e59703f8ebe0c8df120b9278285503d05
Ryan Clare <52092909+rmclare445@users.noreply.github.com>
2022-02-16 18:31:14 -0800

    Update read_nl.py
    Feature idea not necessary, user can just pull from origin.

--- read_nl.py
+++ read_nl.py
@@ -4,7 +4,5 @@ def read_nl( ):
     stream = open( "namelist.yaml", 'r' )
     return yaml.safe_load( stream )
 
-## Maybe add a function to reset nl to default values
-
 if __name__ == "__main__":
     print( read_nl() )
