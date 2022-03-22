6705f580cbb61c55bd52096e6c66042450266470
Ryan Clare <52092909+rmclare445@users.noreply.github.com>
2022-03-22 14:46:35 -0700

    Update write_log.py
    Remove extra argument bug

--- write_log.py
+++ write_log.py
@@ -5,7 +5,7 @@ def write_state( string ):
 def write_err( entry, lt ):
     dtg = "%04d%02d%02d" % ( lt[0], lt[1], lt[2] )      # date time group
     htg = "%02d:%02d:%02d" % ( lt[3], lt[4], lt[5] )    # hour time group
-    with open("log.err" % dtg, "a") as f:
+    with open("log.err", "a") as f:
         f.write( "%s_%s - %s\n" % (dtg, htg, entry) )
 
 def write_ops( stat, lt, temp ):
