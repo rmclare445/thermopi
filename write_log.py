15c9aa6ee371fa1ed07a0ad06e9b029e12ba7c16
Ryan Clare <52092909+rmclare445@users.noreply.github.com>
2022-03-22 14:38:25 -0700

    Add write_ops and write_err
    For operational log to display change-of-status updates.

--- write_log.py
+++ write_log.py
@@ -1,3 +1,16 @@
-def write_log( string ):
+def write_state( string ):
     with open("state.log", "a") as f:
         f.write( string + "\n" )
+
+def write_err( entry, lt ):
+    dtg = "%04d%02d%02d" % ( lt[0], lt[1], lt[2] )      # date time group
+    htg = "%02d:%02d:%02d" % ( lt[3], lt[4], lt[5] )    # hour time group
+    with open("log.err" % dtg, "a") as f:
+        f.write( "%s_%s - %s\n" % (dtg, htg, entry) )
+
+def write_ops( stat, lt, temp ):
+    dtg = "%04d%02d%02d" % ( lt[0], lt[1], lt[2] )      # date time group
+    htg = "%02d:%02d:%02d" % ( lt[3], lt[4], lt[5] )    # hour time group
+    entry = "Furnace on" if stat else "Furnace off"     # change of status entry
+    with open("log.ops_%s" % dtg, "a") as f:
+        f.write( "%s - %s, room temp %s\n" % (htg, entry, temp) )
